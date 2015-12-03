fdadb75c56346fbc206a28ee18212ef3d6523578
Haithem Bel Haj <haythem.belhaj@gmail.com>
2015-12-03 18:24:55 +0100

    implement first version

---
+++ index.js
@@ -0,0 +1,34 @@
+var serveStatic = require('serve-static');
+
+
+module.exports = function(root, rules){
+
+  return function(req, res, next){
+
+    if(!match(req, rules))
+      return next();
+
+    serveStatic(root)(req, res, next);
+  }
+}
+
+
+function match(req, rules){
+
+  return rules.some(function(rule){
+
+    var matches = false;
+
+    req.url = req.url.replace(rule.match, function(match){
+
+      matches = true;
+
+      if(rule.fn)
+        return rule.fn.apply(this, arguments)
+
+      return match;
+    });
+
+    return matches;
+  })
+}
---
+++ test/middleware.js
@@ -0,0 +1,53 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var middleware = require('../index.js');
+
+function noop(){};
+
+describe('static-proxy-middleware tests', function(){
+
+  it('should just call next if rules don\'t match', function(){
+
+    var req = {
+
+      url: '/public/js/test.js'
+    };
+
+    var next = sinon.spy();
+
+    middleware('.', [
+      {
+        match: /publics/g,
+        fn: function(){ return ''}
+      }
+    ])(req, null, next)
+
+
+    expect(next.calledOnce).to.equal(true);
+    expect(req.url).to.equal('/public/js/test.js');
+  })
+
+
+  it('should replace url if rules match', function(){
+
+    var req = {
+
+      url: '/public/js/test.js'
+    };
+
+    var next = sinon.spy();
+
+    middleware('.', [
+      {
+        match: /public/g,
+        fn: function(){ return 'assets'}
+      }
+    ])(req, null, next)
+
+
+    expect(next.calledOnce).to.equal(true);
+    expect(req.url).to.equal('/assets/js/test.js');
+  })
+})
+
+
